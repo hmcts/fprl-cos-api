3c821f371d0c5d367134f6a90e5b499661719592
Patryk Jar <jar.patryk@gmail.com>
2021-06-24 18:38:09 +0200

    builds

--- src/main/java/uk/gov/hmcts/reform/fprl/clients/PbaValidationClient.java
+++ src/main/java/uk/gov/hmcts/reform/fprl/clients/PbaValidationClient.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import uk.gov.hmcts.reform.fprl.config.consts.CustomHttpHeaders;
-import uk.gov.hmcts.reform.fprl.models.payment.PBAOrganisationResponse;
+import uk.gov.hmcts.reform.fprl.models.payment.PbaOrganisationResponse;
 
 @FeignClient(name = "pba-validation-client", url = "${pba.validation.service.api.baseurl}")
 public interface PbaValidationClient {
 
     @ApiOperation("Validates Solicitor Pay By Account (PBA) number for payment")
     @GetMapping(value = "/refdata/external/v1/organisations/pbas")
-    ResponseEntity<PBAOrganisationResponse> retrievePbaNumbers(
+    ResponseEntity<PbaOrganisationResponse> retrievePbaNumbers(
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
         @RequestHeader(CustomHttpHeaders.SERVICE_AUTHORIZATION) String serviceAuthorisation,
         @RequestParam(name = "email") String email);

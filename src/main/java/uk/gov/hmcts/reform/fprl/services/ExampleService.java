377e924e3f38379efa508d4fdbe3597ee99e9165
Patryk Jar <patryk.jar@hmcts.net>
2021-10-13 17:13:11 +0200

    Added feature toggles (#20)

--- src/main/java/uk/gov/hmcts/reform/fprl/services/ExampleService.java
+++ src/main/java/uk/gov/hmcts/reform/fprl/services/ExampleService.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.fprl.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.fprl.config.Features;
 import uk.gov.hmcts.reform.fprl.framework.exceptions.WorkflowException;
 import uk.gov.hmcts.reform.fprl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.fprl.models.dto.ccd.CaseDetails;
@@ -15,8 +16,13 @@ import uk.gov.hmcts.reform.fprl.workflows.ExampleWorkflow;
 public class ExampleService {
 
     private final ExampleWorkflow exampleWorkflow;
+    private final FeatureToggleService featureToggleService;
 
     public CaseData executeExampleWorkflow(CaseDetails caseDetails) throws WorkflowException {
-        return exampleWorkflow.run(caseDetails).getCaseData();
+        if (featureToggleService.isFeatureEnabled(Features.EXAMPLE)) {
+            return exampleWorkflow.run(caseDetails).getCaseData();
+        }
+
+        return caseDetails.getCaseData();
     }
 }
--- src/test/java/uk/gov/hmcts/reform/fprl/services/ExampleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/fprl/services/ExampleServiceTest.java
@@ -5,13 +5,16 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.fprl.config.Features;
 import uk.gov.hmcts.reform.fprl.framework.exceptions.WorkflowException;
 import uk.gov.hmcts.reform.fprl.models.dto.ccd.CaseDetails;
 import uk.gov.hmcts.reform.fprl.utils.CaseDataProvider;
 import uk.gov.hmcts.reform.fprl.utils.CaseDetailsProvider;
 import uk.gov.hmcts.reform.fprl.workflows.ExampleWorkflow;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
@@ -21,17 +24,32 @@ public class ExampleServiceTest {
     @Mock
     private ExampleWorkflow exampleWorkflow;
 
+    @Mock
+    private FeatureToggleService featureToggleService;
+
     @InjectMocks
     private ExampleService exampleService;
 
     @Test
-    public void testToGetCoverage() throws WorkflowException {
+    public void testToGetCoverageExampleFeatureOn() throws WorkflowException {
+        runTestWithExampleFeatureToggleSet(true);
+
+        verify(exampleWorkflow).run(any(CaseDetails.class));
+        verifyNoMoreInteractions(exampleWorkflow);
+    }
+
+    @Test
+    public void testToGetCoverageExampleFeatureOff() throws WorkflowException {
+        runTestWithExampleFeatureToggleSet(false);
+
+        verifyNoInteractions(exampleWorkflow);
+    }
+
+    private void runTestWithExampleFeatureToggleSet(boolean featureToggleFlag) throws WorkflowException {
+        when(featureToggleService.isFeatureEnabled(Features.EXAMPLE)).thenReturn(featureToggleFlag);
         CaseDetails caseDetails = CaseDetailsProvider.of(CaseDataProvider.empty());
         when(exampleWorkflow.run(caseDetails)).thenReturn(caseDetails);
 
         exampleService.executeExampleWorkflow(caseDetails);
-
-        verify(exampleWorkflow).run(caseDetails);
-        verifyNoMoreInteractions(exampleWorkflow);
     }
 }

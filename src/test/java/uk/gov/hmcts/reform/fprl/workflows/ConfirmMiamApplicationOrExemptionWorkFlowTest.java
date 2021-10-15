f7513456ffd3b6d0fe6333b5e3a73f700a656245
pete-lowe <petemlowe@gmail.com>
2021-10-15 14:31:08 +0100

    added further unit test

---
+++ src/test/java/uk/gov/hmcts/reform/fprl/workflows/ConfirmMiamApplicationOrExemptionWorkFlowTest.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.fprl.workflows;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fprl.framework.exceptions.WorkflowException;
+import uk.gov.hmcts.reform.fprl.tasks.ApplicationTimetableEffortsValidationTask;
+import uk.gov.hmcts.reform.fprl.tasks.ApplicationTimetableTimeValidationTask;
+import uk.gov.hmcts.reform.fprl.tasks.ConfirmMiamApplicationOrExemptionTask;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConfirmMiamApplicationOrExemptionWorkFlowTest {
+
+    @Mock private ConfirmMiamApplicationOrExemptionTask confirmMiamApplicationOrExemptionTask;
+
+    @InjectMocks
+    private ConfirmMiamApplicationOrExemptionWorkflow confirmMiamApplicationOrExemptionWorkflow;
+
+    @Test
+    public void whenWorkflowRun_thenExpectedTasksInvoked() throws WorkflowException {
+        confirmMiamApplicationOrExemptionWorkflow.run(
+            CallbackRequest.builder()
+                .caseDetails(CaseDetails.builder()
+                                 .data(ImmutableMap.of())
+                                 .build())
+                .build());
+
+        verify(confirmMiamApplicationOrExemptionTask).execute(any(), any());
+    }
+}

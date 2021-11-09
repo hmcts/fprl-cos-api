f741405274413053f8b26ad125e5f737874e7d63
Yogendra Upasani <yogendra.upasani@hmcts.net>
2021-11-09 15:19:08 +0000

    test secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,22 @@
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = var.location
+
+  tags = var.common_tags
+}
+
+module "key-vault" {
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                    = "fprl-${var.env}"
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
+  common_tags             = var.common_tags
+  create_managed_identity = true
+}
